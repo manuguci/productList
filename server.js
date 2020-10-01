3167c1264cac80c25d381c0ee5b7209f00575cb1
Manuel Gutierrez <manuel.gutierrez@zentragroup.com>
2020-10-01 02:10:51 -0300

    Modify server js

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require ('express');
 const path = require ('path');
+const __dirname = path.resolve();
 const app = express();
-app.use(express.static(_dirname + '/dist/product-list'));
+app.use(express.static(__dirname + '/dist/product-list'));
 app.get('/*', function(req, res){
 
-    res.sendFile(path.join(_dirname + '/dist/product-list/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/product-list/index.html'));
 });
 app.listen(process.env.PORT || 8080);
